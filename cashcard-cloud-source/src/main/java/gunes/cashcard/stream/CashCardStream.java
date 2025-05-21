22c396af4e7794e75fcb3d1b05d1b15ae8747581
gunesserkan <gunesserkan883@gmail.com>
2025-05-21 13:33:19 +0300

    Added StreamBridge

--- cashcard-cloud-source/src/main/java/gunes/cashcard/stream/CashCardStream.java
+++ cashcard-cloud-source/src/main/java/gunes/cashcard/stream/CashCardStream.java
@@ -2,6 +2,7 @@ package gunes.cashcard.stream;
 
 import gunes.cashcard.domain.Transaction;
 import gunes.cashcard.service.DataSourceService;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,12 @@ import java.util.function.Supplier;
 
 @Configuration
 public class CashCardStream {
+
+    private final StreamBridge streamBridge;
+
+    public CashCardStream(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
 
     @Bean
     public Supplier<Transaction> approvalRequest(DataSourceService dataSource){
@@ -20,5 +27,9 @@ public class CashCardStream {
     @Bean
     public DataSourceService dataSourceService(){
         return new DataSourceService();
+    }
+
+    public void publishOnDemand(Transaction transaction){
+        this.streamBridge.send("approval-request-out-0", transaction);
     }
 }
