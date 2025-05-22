a97b53f7158915cc662585e61e721e3b33850927
gunesserkan <gunesserkan883@gmail.com>
2025-05-22 14:46:17 +0300

    Added: Sink module to consume data that produced by Enricher. Updated: .gitignore

---
+++ cashcard-transaction-sink/src/test/java/gunes/cashcardtransactionsink/CashcardTransactionSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package gunes.cashcardtransactionsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardTransactionSinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
