1e5e8bd7b63fd5cbaf6171cae2c883a0ec688f8e
gunesserkan <gunesserkan883@gmail.com>
2025-05-22 22:11:18 +0300

    Added: Sink to file method

--- cashcard-transaction-sink/src/main/java/gunes/cashcardtransactionsink/sink/CashCardTransactionSink.java
+++ cashcard-transaction-sink/src/main/java/gunes/cashcardtransactionsink/sink/CashCardTransactionSink.java
@@ -5,15 +5,51 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import gunes.domain.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.StringJoiner;
+import java.nio.file.StandardOpenOption;
+
 import java.util.function.Consumer;
 
 @Configuration
 public class CashCardTransactionSink {
 
+
+    public static final String CSV_FILE_PATH = System.getProperty("user.dir") + "/build/tmp/transactions-output.csv";
+
     @Bean
-    public Consumer<EnrichedTransaction> sinkToConsole(){
+    public Consumer<EnrichedTransaction> sinkToConsole() {
         return enrichedTransaction -> {
-            System.out.println("Transaction Received: "+ enrichedTransaction);
+            System.out.println("Transaction Received: " + enrichedTransaction);
         };
+    }
+
+    @Bean
+    public Consumer<EnrichedTransaction> cashCardTransactionFileSink() {
+        return enrichedTransaction -> {
+            StringJoiner joiner = new StringJoiner(",");
+            StringJoiner enrichedTxnTextual = joiner.add(String.valueOf(enrichedTransaction.id()))
+                    .add(String.valueOf(enrichedTransaction.cashCard().id()))
+                    .add(String.valueOf(enrichedTransaction.cashCard().amountRequestedForAuth()))
+                    .add(enrichedTransaction.cardHolderData().name())
+                    .add(enrichedTransaction.cardHolderData().userId().toString())
+                    .add(enrichedTransaction.cardHolderData().address())
+                    .add(enrichedTransaction.approvalStatus().name());
+            Path path = Paths.get(CSV_FILE_PATH);
+            try {
+                ensureSinkFileExists();
+                Files.writeString(path, enrichedTxnTextual.toString() + "\n", StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private void ensureSinkFileExists() throws IOException {
+        new File(CSV_FILE_PATH).createNewFile();
     }
 }
